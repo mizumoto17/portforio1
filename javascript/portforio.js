4587aa7beaef467461ad86016c143e74217b4244
mizumoto <sprashbrothers417@gmail.com>
2022-03-25 19:38:01 +0900

    add new file

---
+++ javascript/portforio.js
@@ -0,0 +1,20 @@
+pagetop//-ie-safari not
+const pagetopBtn = document.querySelector("#pagetop");
+pagetopBtn.addEventListener('click',() => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+// $(function() {
+//   let pagetopBtn = $('#pagetop');
+//   pagetopBtn.hide();
+
+//   pagetopBtn.click(function () {
+//     $('body,html').animate({
+//       scrolltop: 0
+//     }, 500);
+//     return false;
+//   });
+// });
